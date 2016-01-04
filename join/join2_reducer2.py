7567e75eafc01cdc1945abe7bacd6b8cb444acf0
Hang Shi <hangshiisi@gmail.com>
2016-01-04 00:37:28 -0800

    correct answer for part 2

--- join/join2_reducer2.py
+++ join/join2_reducer2.py
@@ -25,16 +25,14 @@ import sys
 
 prev_word          = "  "                #initialize previous word  to blank string
 ABC_CHANNEL 	   = 'ABC'
-
-curr_word_total_cnt = 0 
-running_total      = 0
-line_cnt           = 0  #count input lines
 abc_found = False 
+running_total      = 0
+line_cnt = 0 
 
 for line in sys.stdin:
     line       = line.strip()       #strip out carriage return
     key_value  = line.split('\t')   #split line, into key and value, returns a list
-    line_cnt   = line_cnt+1     
+    line_cnt = line_cnt + 1
 
     #note: for simple debugging use print statements, ie:  
     curr_word  = key_value[0]         #key is first item in list, indexed by 0
@@ -45,26 +43,20 @@ for line in sys.stdin:
     #   (b/c for the first line the previous word is not applicable)
     #   if so then print out list of dates and counts
     #----------------------------------------------------
-    if curr_word != prev_word :
-        # print curr_word, prev_word, value_in
+    if curr_word != prev_word:
         if abc_found: 
-	    print('{0} {1}'.format(prev_word,curr_word_total_cnt))
-
-        if value_in.isdigit(): 
-            abc_found = False         
-            curr_word_total_cnt = int(value_in)
+	   print('{0} {1}'.format(prev_word,curr_word_total_cnt))
+        if value_in[0:2] == ABC_CHANNEL[0:2]: 
+            abc_found = True
         else: 
-            abc_found = True 
-            curr_word_total_cnt = 0
-             
-        prev_word         = curr_word  
+            abc_found = False
+	curr_word_total_cnt = 0 
+        prev_word           = curr_word  #set up previous word for the next set of input lines
     else: 
-        if value_in.isdigit(): 
-            curr_word_total_cnt += int(value_in) 
-        else: 
-            abc_found = True 
- 
-#print last one 
+        curr_word_total_cnt += int(value_in) 
+
 if abc_found: 
-    print('{0} {1}'.format(prev_word,curr_word_total_cnt))
+   print('{0} {1}'.format(prev_word,curr_word_total_cnt))
+	
+
 
